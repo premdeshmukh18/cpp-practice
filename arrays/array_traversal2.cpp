a020db8b4a0c268c9d34425bcf2c51808bde129e
Prem Deshmukh <Premdeshmukh1818@gmail.com>
2022-06-25 14:46:46 +0530

    array

---
+++ arrays/array_traversal2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={2,3,2,2,32,3,2,3,};
+    for(int x : arr)
+    cout<<x<<endl;
+    return 0;
+
+//this is range based for loop array traversal.
+}
